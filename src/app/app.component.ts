35a2d0cd9c04b8e8414b3213f8926b6e9f89303e
Lexuch <72134534+Lexuch@users.noreply.github.com>
2020-11-13 23:39:26 +0300

    test

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'test2';
+
+  firstOperand: number;
+  secondOperand: number;
+  operator1: string;
+  operator2: string;
+  result: boolean;
+  errorMessage: string;
+
+  doCalc(){
+      if(this.firstOperand != null && this.secondOperand != null  && this.operator2 != null){
+        if(this.operator1!='!'){
+          if(this.operator2=='&&'){
+            this.result = Boolean(this.firstOperand && this.secondOperand)
+            this.errorMessage = 'Нет ошибок'
+          }else if(this.operator2=='||'){
+            this.result = Boolean(this.firstOperand || this.secondOperand)
+            this.errorMessage = 'Нет ошибок'
+          }
+        }else if (this.operator1 =='!'){
+          if(this.operator2=='&&'){
+            this.result = !Boolean(this.firstOperand && this.secondOperand)
+            this.errorMessage = 'Нет ошибок'
+          }else if(this.operator2=='||'){
+            this.result = !Boolean(this.firstOperand || this.secondOperand)
+            this.errorMessage = 'Нет ошибок'
+          }
+        }
+      }else if(this.firstOperand==null){
+        this.errorMessage='Нет первого операнда'
+      }else if(this.secondOperand==null){
+        this.errorMessage='Нет второго операнда'
+      }else if(this.operator2==null){
+        this.errorMessage='Нет оператора действия'
+      }
+    }
+}
